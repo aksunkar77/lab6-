880295039e81a9bf268febdd7e7f85a0ba3a8d76
aksunkar77 <a_serekbol@kbtu.kz>
2025-03-15 15:34:12 +0500

    Update letters.py

--- letters.p/letters.py
+++ letters.p/letters.py
@@ -7,8 +7,7 @@ if not os.path.exists(directory_folder):
     os.makedirs(directory_folder)
 
 for letter in string.ascii_uppercase:
-    file_path = os.path.join(directory_folder, f"{letter}.txt")
+    file_path = os.path.join(directory_folder, f"{letter.lower()}.txt")  
     with open(file_path, 'w') as file:
-        file.write(f"{letter}.txt was created successfully\n")
-
+        file.write(f"{letter.lower()}.txt was created successfully\n")  
 print("All files created successfully!")
